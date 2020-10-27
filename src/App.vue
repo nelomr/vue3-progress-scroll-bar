c9a01a98af774c33cd63ad6be5839417e97dd9da
nelomr <nelomr9@gmail.com>
2020-10-27 19:43:06 +0100

    Updates

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,62 @@
 <template>
-  <ProgressBar />
+  <header>
+    <img src="@/assets/logo.png" alt="">
+    <h2 v-text="dummyText.title"></h2>
+    <ProgressBar :height="10" />
+  </header>
+  <div class="content" v-html="dummyText.content"></div>
 </template>
 
 <script>
-import ProgressBar from './components/ProgressBar.vue'
+import ProgressBar from "./components/ProgressBar.vue";
+import dummyText from "./data/dummyText.json";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    ProgressBar
-  }
-}
+    ProgressBar,
+  },
+  data() {
+    return {
+      dummyText
+    };
+  },
+};
 </script>
 
-<style>
+<style lang="postcss">
+html,
+body {
+  padding: 0;
+  margin: 0;
+}
+
 #app {
+  margin-top: 120px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  height: 4000px;
+}
+
+header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 30px 10px 10px;
+  background: #EEE;
+}
+
+img {
+  max-width: 70px;
+}
+
+.content p {
+  margin: 0 10px 1em;
+  text-align: left;
 }
 </style>
--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,66 +1,72 @@
 <template>
-    <div class="progress-bar--container" :style="`${positionValue};`">
-        <div
-            v-scroll
-            :style="`background-color: ${colorValue};`"
-            class="progress-bar"
-            >
-        </div>
-    </div>
+  <div
+    class="progress-bar--container"
+    :style="`${positionValue}; height: ${height}px`"
+  >
+    <div
+      :style="`background-color: ${colorValue}; width: ${width}%;`"
+      class="progress-bar"
+    ></div>
+  </div>
 </template>
 <script>
-import {computed} from 'vue';
+import { computed, ref, onMounted } from "vue";
 
 export default {
-    props: {
-        color: {
-            type: String,
-            default: '#000'
-        },
-        position: {
-            type: String,
-            default: 'top'
-        }
+  props: {
+    color: {
+      type: String,
+      default: "#41B883",
     },
-    setup(props) {
-        const colorValue = computed(() => (props.color));
-        const positionValue = computed(() => {
-            let position = props.position;
-            if (position === 'top' || position === 'bottom') {
-                return `${position}: 0`;
-            }
-            return 'top: 0';
-        });
+    position: {
+      type: String,
+      default: "top",
+    },
+    height: {
+      type: String,
+      default: "5",
+    },
+  },
+  setup(props) {
+    const width = ref(0);
+    const colorValue = computed(() => props.color);
+    const positionValue = computed(() => {
+      let position = props.position;
+      if (position === "top" || position === "bottom") {
+        return `${position}: 0`;
+      }
+      return "top: 0";
+    });
 
-        return {colorValue, positionValue};
-    },
-    directives: {
-        scroll: {
-            mounted(el) {
-                document.addEventListener('scroll', () => {
-                    const documentHeight = (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-                    const width = (window.scrollY*100) / documentHeight;
-                    el.style['width'] = `${width}%`;
-                });
-            }
-        }
+    function progress() {
+      let documentHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+      width.value = (window.scrollY * 100) / documentHeight;
     }
-}
+    onMounted(() => {
+      document.addEventListener("scroll", function() {
+        progress();
+      });
+    });
+
+    return { colorValue, width, positionValue };
+  }
+};
 </script>
 
-<style lang="postcss" scoped>
-    .progress-bar--container {
-        position: fixed;
-        left: 0;
-        right: 0;
-        width: 100%;
-        height: 5px;
-    }
+<style lang="postcss">
+.progress-bar--container {
+  position: fixed;
+  left: 0;
+  right: 0;
+  width: 100%;
+}
 
-    .progress-bar {
-        position: relative;
-        width: 0;
-        height: 100%;
-        transition: width .1s ease;
-    }
+.progress-bar {
+  position: relative;
+  width: 0;
+  height: 100%;
+  transition: width 0.1s ease;
+}
 </style>
